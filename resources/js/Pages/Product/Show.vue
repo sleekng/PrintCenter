a57ced3115b0cbee24ebfa3a90c695ef16991c1a
sleekng <153427570+sleekng@users.noreply.github.com>
2024-07-06 09:25:04 +0100

    7/6

--- resources/js/Pages/Product/Show.vue
+++ resources/js/Pages/Product/Show.vue
@@ -170,7 +170,7 @@
                             <option value="10">10</option>
                             <option value="20">20</option>
                             <option value="30">30</option>
-                            <option value="4">40</option>
+                            <option value="40">40</option>
                             <option value="50">50</option>
                             <option value="100">100</option>
                             <option value="500">500</option>
